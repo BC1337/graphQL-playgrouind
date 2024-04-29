5bcbe0ca2eaf15ad13d9fc3c1b502694c8ca2688
BC1337 <brettcomrie@gmail.com>
2024-04-29 11:27:17 -0500

    Delteing data with apollo cache

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,29 @@ import {
   InMemoryCache, 
 } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        }
+      }
+    }
+  }
+});
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache
 });
 
 
